b1f9430021de0dfae43a8f0cf27c36b2600f6bab
kshultzCB <kshultz@cloudbees.com>
2019-02-08 18:09:39 -0500

    Move checkout master out of loop

--- delete-branches.sh
+++ delete-branches.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
+git checkout master
 for i in `seq 1 5`;
     do
-        # git checkout -b "automated-branch-$i"
-        # echo "node () { " > Jenkinsfile
-        # echo "  echo \"Hello from pass $i\"" >> Jenkinsfile
-        # echo "  sh \"netstat -a\" " >> Jenkinsfile
-        # echo "}" >> Jenkinsfile
-        # git add Jenkinsfile
-        # git commit -m "Creating commit for iteration $i"
-        # hub pull-request -p -m "command line pull request for iteration $i"
-        # echo "Sleep 30 seconds"
-        # sleep 30
-        git checkout master
-        git push --delete "origin automated-branch-$i"
+        git push --delete origin "automated-branch-$i"
         git branch -D "automated-branch-$i"
-
     done 
