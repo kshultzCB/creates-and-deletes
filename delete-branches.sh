0591e895e2905d37ce36e2824279ac02996d4f96
kshultzCB <kshultz@cloudbees.com>
2019-02-08 17:57:12 -0500

    Add the scripts

---
+++ delete-branches.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in `seq 1 4`;
+    do
+        # git checkout -b "automated-branch-$i"
+        # echo "node () { " > Jenkinsfile
+        # echo "  echo \"Hello from pass $i\"" >> Jenkinsfile
+        # echo "  sh \"netstat -a\" " >> Jenkinsfile
+        # echo "}" >> Jenkinsfile
+        # git add Jenkinsfile
+        # git commit -m "Creating commit for iteration $i"
+        # hub pull-request -p -m "command line pull request for iteration $i"
+        # echo "Sleep 30 seconds"
+        # sleep 30
+        git checkout master
+        git push --delete origin automated-branch-$i
+        git branch -d automated-branch-$i
+
+    done 
