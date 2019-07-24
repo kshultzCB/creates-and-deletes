ee89c165dc73c37fa4c7eaf44cd8e2ae7758b98a
Karl Shultz <kshultz@cloudbees.com>
2019-07-24 13:26:21 -0400

    Have it take an argument

--- create-pull-requests.sh
+++ create-pull-requests.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "No PREFIX supplied"
+    exit
+fi
+PREFIX=$1
+echo "Prefix for new branches will be $PREFIX"
 git checkout master
-for i in `seq 1 3`;
+for i in `seq 20 22`;
     do
-        git checkout -b "automated-branch-$i"
+        git checkout -b "$PREFIX-$i"
         echo "node () { " > Jenkinsfile
-        echo "  echo \"Hello from iteration $i\"" >> Jenkinsfile
+        echo "  echo \"Branch is $PREFIX-$i\"" >> Jenkinsfile
         echo "  sh \"netstat -a\" " >> Jenkinsfile
         echo "}" >> Jenkinsfile
         git add Jenkinsfile
-        git commit -m "Creating commit for iteration $i"
-        hub pull-request -p -m "command line pull request for iteration $i"
+        git commit -m "Commit iteration number $i for branch $PREFIX-$i"
+        hub pull-request -p -m "Creating PR from branch $PREFIX-$i"
         echo "Sleep 2 seconds"
         sleep 2
     done 
